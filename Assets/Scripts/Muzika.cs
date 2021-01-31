b9025110efbe5440b2517b6864d0f523b564f20f
Miljan <bebinaflasica@gmail.com>
2021-01-31 16:30:03 +0100

    Muzika

---
+++ Assets/Scripts/Muzika.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzika : MonoBehaviour
+{
+
+    private static Muzika theMusic = null;
+
+    private void Awake()
+    {
+        if (theMusic != null)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            theMusic = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
